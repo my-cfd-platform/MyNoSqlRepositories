d200ce7a55d193a177632d91101fb31d2ca2b1da
amigin <amigin@gmail.com>
2019-11-09 12:57:57 +0200

    Added Factory of Repos

--- SimpleTrading.MyNoSqlRepositories/MyNoSqlServerUtils.cs
+++ SimpleTrading.MyNoSqlRepositories/MyNoSqlServerUtils.cs
@@ -3,6 +3,15 @@ using MyNoSqlClient.ReadRepository;
 
 namespace SimpleTrading.MyNoSqlRepositories
 {
+
+    public static class MyNoSqlServerFactory
+    {
+        public static BidAskMyNoSqlCache CreateBidAskMyNoSqlCache(this IMyNoSqlConnection connection)
+        {
+            return new BidAskMyNoSqlCache(connection.ToMyNoSqlReadRepository<BidAskMyNoSqlTableEntity>("quoteprofile"));
+        }
+    }
+    
     public static class MyNoSqlServerUtils
     {
         public static IMyNoSqlReadRepository<T> ToMyNoSqlReadRepository<T>(this IMyNoSqlConnection connection, string tableName) where T : IMyNoSqlTableEntity
