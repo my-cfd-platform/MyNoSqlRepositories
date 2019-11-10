bbd186368e7a3f1dd3ce200d37ea67cc2ee1dc2d
amigin <amigin@gmail.com>
2019-11-10 11:39:31 +0200

    Added Support of TradingGroups

--- SimpleTrading.MyNoSqlRepositories/MyNoSqlServerUtils.cs
+++ SimpleTrading.MyNoSqlRepositories/MyNoSqlServerUtils.cs
@@ -1,6 +1,7 @@
 using MyNoSqlClient;
 using MyNoSqlClient.ReadRepository;
 using SimpleTrading.MyNoSqlRepositories.Instruments;
+using SimpleTrading.MyNoSqlRepositories.TradingGroups;
 
 namespace SimpleTrading.MyNoSqlRepositories
 {
@@ -11,25 +12,44 @@ namespace SimpleTrading.MyNoSqlRepositories
         {
             return new BidAskMyNoSqlCache(connection.ToMyNoSqlReadRepository<BidAskMyNoSqlTableEntity>("quoteprofile"));
         }
+
+        private const string InstrumentsTable = "instruments";
         
         public static InstrumentsMyNoSqlReadCache CreateInstrumentsMyNoSqlReadCache(this IMyNoSqlConnection connection)
         {
-            return new InstrumentsMyNoSqlReadCache(connection.ToMyNoSqlReadRepository<TradingInstrumentMyNoSqlEntity>("instruments"));
+            return new InstrumentsMyNoSqlReadCache(connection.ToMyNoSqlReadRepository<TradingInstrumentMyNoSqlEntity>(InstrumentsTable));
         }
         
         public static TradingInstrumentsMyNoSqlRepository CreateTradingInstrumentsMyNoSqlRepository(this IMyNoSqlConnection connection)
         {
-            return new TradingInstrumentsMyNoSqlRepository(new MyNoSqlServerClient<TradingInstrumentMyNoSqlEntity>(connection, "instruments"));
+            return new TradingInstrumentsMyNoSqlRepository(
+                new MyNoSqlServerClient<TradingInstrumentMyNoSqlEntity>(connection, InstrumentsTable));
         }
-        
+
+
+        private const string TradingGroupsTable = "trading-groups";
+        public static TradingGroupsMyNoSqlReader CreateTradingGroupsMyNoSqlReader(this IMyNoSqlConnection connection)
+        {
+            return new TradingGroupsMyNoSqlReader(connection.ToMyNoSqlReadRepository<TradingGroupMyNoSqlEntity>(TradingGroupsTable));
+        }
+
+        public static TradingGroupsMyNoSqlRepository CreateTradingGroupsMyNoSqlRepository(
+            this IMyNoSqlConnection connection)
+        {
+            return new TradingGroupsMyNoSqlRepository(
+                new MyNoSqlServerClient<TradingGroupMyNoSqlEntity>(connection, TradingGroupsTable));
+        }
+
     }
     
     public static class MyNoSqlServerUtils
     {
-        public static IMyNoSqlReadRepository<T> ToMyNoSqlReadRepository<T>(this IMyNoSqlConnection connection, string tableName) where T : IMyNoSqlTableEntity
+        public static IMyNoSqlReadRepository<T> ToMyNoSqlReadRepository<T>(this IMyNoSqlConnection connection, string tableName) 
+            where T : IMyNoSqlTableEntity
         {
             return new MyNoSqlReadRepository<T>(connection, tableName);
         }  
+        
  
     }
     
