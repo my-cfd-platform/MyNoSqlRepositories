a21db5c62e6ae6a96363de8d0f235d74bf0f2574
eugenKo <jecacs1@gmail.com>
2019-12-17 15:46:54 +0000

    Refactor/remove old entity

--- SimpleTrading.MyNoSqlRepositories/MyNoSqlServerUtils.cs
+++ SimpleTrading.MyNoSqlRepositories/MyNoSqlServerUtils.cs
@@ -1,8 +1,6 @@
 using MyNoSqlClient;
 using MyNoSqlClient.ReadRepository;
 using SimpleTrading.MyNoSqlRepositories.Countries;
-using SimpleTrading.MyNoSqlRepositories.Instruments;
-using SimpleTrading.MyNoSqlRepositories.TradingGroups;
 
 namespace SimpleTrading.MyNoSqlRepositories
 {
@@ -11,33 +9,6 @@ namespace SimpleTrading.MyNoSqlRepositories
         public static BidAskMyNoSqlCache CreateBidAskMyNoSqlCache(this IMyNoSqlConnection connection)
         {
             return new BidAskMyNoSqlCache(connection.ToMyNoSqlReadRepository<BidAskMyNoSqlTableEntity>("quoteprofile"));
-        }
-
-        private const string InstrumentsTable = "instruments";
-        
-        public static InstrumentsMyNoSqlReadCache CreateInstrumentsMyNoSqlReadCache(this IMyNoSqlConnection connection)
-        {
-            return new InstrumentsMyNoSqlReadCache(connection.ToMyNoSqlReadRepository<TradingInstrumentMyNoSqlEntity>(InstrumentsTable));
-        }
-        
-        public static TradingInstrumentsMyNoSqlRepository CreateTradingInstrumentsMyNoSqlRepository(this IMyNoSqlConnection connection)
-        {
-            return new TradingInstrumentsMyNoSqlRepository(
-                new MyNoSqlServerClient<TradingInstrumentMyNoSqlEntity>(connection, InstrumentsTable));
-        }
-
-
-        private const string TradingGroupsTable = "trading-groups";
-        public static TradingGroupsMyNoSqlReader CreateTradingGroupsMyNoSqlReader(this IMyNoSqlConnection connection)
-        {
-            return new TradingGroupsMyNoSqlReader(connection.ToMyNoSqlReadRepository<TradingGroupMyNoSqlEntity>(TradingGroupsTable));
-        }
-
-        public static TradingGroupsMyNoSqlRepository CreateTradingGroupsMyNoSqlRepository(
-            this IMyNoSqlConnection connection)
-        {
-            return new TradingGroupsMyNoSqlRepository(
-                new MyNoSqlServerClient<TradingGroupMyNoSqlEntity>(connection, TradingGroupsTable));
         }
 
         private const string CountriesTable = "countries";
@@ -59,9 +30,6 @@ namespace SimpleTrading.MyNoSqlRepositories
             where T : IMyNoSqlTableEntity
         {
             return new MyNoSqlReadRepository<T>(connection, tableName);
-        }  
-        
- 
+        }
     }
-    
 }
