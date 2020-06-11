c6cf2261040d665446b6e9a0a4a0b483430845b6
eugenKo <jecacs1@gmail.com>
2020-06-11 21:30:38 +0300

    updated SimpleTrading.Abstraction

--- SimpleTrading.MyNoSqlRepositories/Reports/ActivePositions/ReportActivePositionMyNoSqlRepository.cs
+++ SimpleTrading.MyNoSqlRepositories/Reports/ActivePositions/ReportActivePositionMyNoSqlRepository.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using MyNoSqlServer.Abstractions;
 using SimpleTrading.Abstraction.Reports.ActivePositions;
@@ -12,6 +13,21 @@ namespace SimpleTrading.MyNoSqlRepositories.Reports.ActivePositions
         public ReportActivePositionMyNoSqlRepository(IMyNoSqlServerDataWriter<ReportActivePositionMyNoSqlEntity> table)
         {
             _table = table ?? throw new ArgumentNullException(nameof(table));
+        }
+
+        public async Task<IEnumerable<IActivePositionsSnapshot>> GetAsync()
+        {
+            var pk = ReportActivePositionMyNoSqlEntity.GeneratePartitionKey();
+
+            return await _table.GetAsync(pk);
+        }
+
+        public async Task<IEnumerable<IActivePositionsSnapshot>> GetAsync(string accountId)
+        {
+            var pk = ReportActivePositionMyNoSqlEntity.GeneratePartitionKey();
+            var rk = ReportActivePositionMyNoSqlEntity.GenerateRowKey(accountId);
+
+            return await _table.GetAsync(pk);        
         }
 
         public async Task SaveAsync(IActivePositionsSnapshot snapshot)
