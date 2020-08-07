c69aa1630c3ad044ecee9a09b764b90763d93d6c
eugenKo <jecacs1@gmail.com>
2020-08-07 10:36:53 +0300

    1.2.21 - updated EmailTemplatesEntity

--- SimpleTrading.MyNoSqlRepositories/Emails/EmailTemplate/EmailTemplateMyNoSqlEntity.cs
+++ SimpleTrading.MyNoSqlRepositories/Emails/EmailTemplate/EmailTemplateMyNoSqlEntity.cs
@@ -19,7 +19,7 @@ namespace SimpleTrading.MyNoSqlRepositories.Emails.EmailTemplate
         
         public string BrandId => RowKey;
         
-        public string RedirectUrl { get; set; }
+        public string ConfirmationUrl { get; set; }
 
         public EmailTypes EmailType { get; set; }
         
@@ -31,7 +31,7 @@ namespace SimpleTrading.MyNoSqlRepositories.Emails.EmailTemplate
         
         public string TokenExpires { get; set; }
 
-        public static EmailTemplatesMyNoSqlEntity Create(string brandId, EmailTypes emailType, Languages language, string templateId, string subject, string tokenExpires, string redirectUrl)
+        public static EmailTemplatesMyNoSqlEntity Create(string brandId, EmailTypes emailType, Languages language, string templateId, string subject, string tokenExpires, string confirmationUrl)
         {
             return new EmailTemplatesMyNoSqlEntity
             {
@@ -42,7 +42,7 @@ namespace SimpleTrading.MyNoSqlRepositories.Emails.EmailTemplate
                 TemplateId = templateId,
                 Subject = subject,
                 TokenExpires = tokenExpires,
-                RedirectUrl = redirectUrl
+                ConfirmationUrl = confirmationUrl
             };
         }
     }
