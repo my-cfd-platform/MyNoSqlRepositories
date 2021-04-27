b45414a8e1cdf9099de64ab2d3a574847403b3c7
Mikhail.K <Mikhail.K@DESKTOP-6AOVMQA>
2021-04-27 15:04:33 +0300

    Added TraderOnboarding entity

---
+++ SimpleTrading.MyNoSqlRepositories/Misc/TraderOnboarding/TraderOnboardingMyNoSqlTableEntity.cs
@@ -0,0 +1,22 @@
+﻿using MyNoSqlServer.Abstractions;
+using SimpleTrading.Abstraction.Misc.TraderOnboarding;
+
+namespace SimpleTrading.MyNoSqlRepositories.Misc.Onboarding
+{
+    public class TraderOnboardingMyNoSqlTableEntity : MyNoSqlDbEntity, ITraderOnboarding
+    {
+        public short Id { get; set; }
+
+        public static string GeneratePartitionKey() => "id";
+
+        public static string GenerateRowKey() => "id";
+
+        public static TraderOnboardingMyNoSqlTableEntity Create(ITraderOnboarding onboarding) =>
+            new TraderOnboardingMyNoSqlTableEntity
+            {
+                PartitionKey = GeneratePartitionKey(),
+                RowKey = GenerateRowKey(),
+                Id = onboarding.Id
+            };
+    }
+}
