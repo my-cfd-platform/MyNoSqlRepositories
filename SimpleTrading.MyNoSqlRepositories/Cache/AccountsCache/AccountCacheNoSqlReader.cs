af9552f9acc1e09a870a5b3ba67c4e7e6a1c74e7
Hays <HaysR>
2021-05-18 16:38:03 +0300

    added accounts cache writer

--- SimpleTrading.MyNoSqlRepositories/Cache/AccountsCache/AccountCacheNoSqlReader.cs
+++ SimpleTrading.MyNoSqlRepositories/Cache/AccountsCache/AccountCacheNoSqlReader.cs
@@ -1,27 +1,28 @@
 using System;
 using System.Collections.Generic;
 using MyNoSqlServer.Abstractions;
+using SimpleTrading.Abstraction.Accounts;
 using SimpleTrading.MyNoSqlRepositories.Engine;
 
 namespace SimpleTrading.MyNoSqlRepositories.Cache.AccountsCache
 {
-    public class AccountCacheNoSqlReader
+    public class AccountCacheNoSqlReader : IAccountCacheReader
     {
-        private readonly IMyNoSqlServerDataReader<EnginePersistenceQueueItemMyNoSqlModel> _reader;
+        private readonly IMyNoSqlServerDataReader<AccountsCacheNoSqlEntity> _reader;
 
-        public AccountCacheNoSqlReader(IMyNoSqlServerDataReader<EnginePersistenceQueueItemMyNoSqlModel> reader)
+        public AccountCacheNoSqlReader(IMyNoSqlServerDataReader<AccountsCacheNoSqlEntity> reader)
         {
             _reader = reader;
         }
 
-        public IReadOnlyList<EnginePersistenceQueueItemMyNoSqlModel> Get(string accountId)
+        public IReadOnlyList<ITradingAccount> Get(string accountId)
         {
             var pk = EnginePersistenceQueueItemMyNoSqlModel.GeneratePartitionKey(accountId);
 
             return _reader.Get(pk);
         }
 
-        public EnginePersistenceQueueItemMyNoSqlModel Get(string accountId, DateTime dateTime)
+        public ITradingAccount Get(string accountId, DateTime dateTime)
         {
             var pk = EnginePersistenceQueueItemMyNoSqlModel.GeneratePartitionKey(accountId);
             var rk = EnginePersistenceQueueItemMyNoSqlModel.GenerateRowKey(dateTime);
@@ -29,8 +30,8 @@ namespace SimpleTrading.MyNoSqlRepositories.Cache.AccountsCache
             return _reader.Get(pk, rk);
         }
 
-        public void BindEventSubscribers(Action<IReadOnlyList<EnginePersistenceQueueItemMyNoSqlModel>> updateCallback,
-            Action<IReadOnlyList<EnginePersistenceQueueItemMyNoSqlModel>> deleteCallback)
+        public void BindEventSubscribers(Action<IReadOnlyList<ITradingAccount>> updateCallback,
+            Action<IReadOnlyList<ITradingAccount>> deleteCallback)
         {
             _reader.SubscribeToUpdateEvents(updateCallback, deleteCallback);
         }
